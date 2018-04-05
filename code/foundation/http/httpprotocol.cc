7fded564354f9071f1e8c3f7db9477f28bd00d27
johannes <johannes.hirche@gmail.com>
2018-04-05 21:19:26 +0200

    - added generated nidl protocols for http until we have a better solution for nidl things in place

---
+++ code/foundation/http/httpprotocol.cc
@@ -0,0 +1,19 @@
+//------------------------------------------------------------------------------
+//  MACHINE GENERATED, DON'T EDIT!
+//------------------------------------------------------------------------------
+#include "stdneb.h"
+#include "httpprotocol.h"
+
+
+namespace Http
+{
+    __ImplementClass(Http::AttachRequestHandler, 'arqh', Messaging::Message);
+    __ImplementMsgId(AttachRequestHandler);
+    __ImplementClass(Http::RemoveRequestHandler, 'rrqh', Messaging::Message);
+    __ImplementMsgId(RemoveRequestHandler);
+} // Http
+
+namespace Commands
+{
+} // namespace Commands
+//------------------------------------------------------------------------------
