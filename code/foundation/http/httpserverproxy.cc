eb8e44bac0acc191e0edbd12c9338d3ff152ba4f
johannes <johannes.hirche@gmail.com>
2018-04-05 21:36:06 +0200

    - fixed pch includes and added httpprotocol to CMakeLists.txt

--- code/foundation/http/httpserverproxy.cc
+++ code/foundation/http/httpserverproxy.cc
@@ -3,7 +3,7 @@
 //  (C) 2008 Radon Labs GmbH
 //  (C) 2013-2016 Individual contributors, see AUTHORS file
 //------------------------------------------------------------------------------
-#include "stdneb.h"
+#include "foundation/stdneb.h"
 #include "http/httpserverproxy.h"
 #include "http/httpprotocol.h"
 #include "http/httpinterface.h"
