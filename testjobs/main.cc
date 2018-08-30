015dba60433d93a29880903a7307832ebd880107
Gustav Sterbrant <gustav.sterbrant@gmail.com>
2018-08-31 01:50:14 +0200

    Added new test for new jobs.

---
+++ testjobs/main.cc
@@ -0,0 +1,37 @@
+//------------------------------------------------------------------------------
+//  testmem/main.cc
+//  (C) 2006 Radon Labs GmbH
+//------------------------------------------------------------------------------
+#include "stdneb.h"
+#include "system/appentry.h"
+#include "core/coreserver.h"
+#include "testbase/testrunner.h"
+#include "jobstest.h"
+
+using namespace Core;
+using namespace Test;
+
+
+void
+__cdecl main()
+{
+	// create Nebula3 runtime
+	Ptr<CoreServer> coreServer = CoreServer::Create();
+	coreServer->SetAppName(Util::StringAtom("Nebula Jobs Tests"));
+	coreServer->Open();
+
+	n_printf("NEBULA JOBS TESTS\n");
+	n_printf("========================\n");
+
+	// setup and run test runner
+	Ptr<TestRunner> testRunner = TestRunner::Create();
+	testRunner->AttachTestCase(JobsTest::Create());
+	testRunner->Run();
+	//testRunner->AttachTestCase(BXmlReaderTest::Create());
+
+	coreServer->Close();
+	coreServer = nullptr;
+	testRunner = nullptr;
+
+	Core::SysFunc::Exit(0);
+}
