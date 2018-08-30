7a780f0f95d05430d59c4af587749fb6ec15288c
Gustav Sterbrant <gustav.sterbrant@gmail.com>
2018-08-31 01:52:42 +0200

    Created material interface with the same design mentality as the id allocated objects.

---
+++ code/render/materials/material.cc
@@ -0,0 +1,47 @@
+//------------------------------------------------------------------------------
+//  material.cc
+//  (C) 2018 Individual contributors, see AUTHORS file
+//------------------------------------------------------------------------------
+#include "stdneb.h"
+#include "material.h"
+#include "materialtype.h"
+#include "materialpool.h"
+namespace Materials
+{
+
+MaterialPool* materialPool = nullptr;
+MaterialType* currentType = nullptr;
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+MaterialBeginBatch(MaterialType* type, CoreGraphics::BatchGroup::Code batch)
+{
+	n_assert(type != nullptr);
+	currentType = type;
+	currentType->BeginBatch(batch);
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+MaterialApply(const Resources::ResourceId& mat)
+{
+	n_assert(materialPool != nullptr);
+	currentType->ApplyInstance(materialPool->GetId(mat));
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+MaterialEndBatch()
+{
+	n_assert(currentType != nullptr);
+	currentType->EndBatch();
+	currentType = nullptr;
+}
+
+} // namespace Material
---
+++ code/render/materials/material.h
@@ -0,0 +1,33 @@
+#pragma once
+//------------------------------------------------------------------------------
+/**
+	Materials represent a set of settings and a correlated type, which tells the engine which shader to use and how
+
+	(C) 2018 Individual contributors, see AUTHORS file
+*/
+//------------------------------------------------------------------------------
+#include "ids/id.h"
+#include "ids/idallocator.h"
+#include "resources/resourceid.h"
+#include "coregraphics/batchgroup.h"
+#include <functional>
+namespace Materials
+{
+
+class MaterialType;
+ID_32_TYPE(MaterialTypeId);
+ID_32_TYPE(MaterialId);
+
+
+/// begin a material batch
+void MaterialBeginBatch(MaterialType* type, CoreGraphics::BatchGroup::Code batch);
+/// apply instance of material
+void MaterialApply(const Resources::ResourceId& mat);
+/// end a material batch
+void MaterialEndBatch();
+
+extern MaterialType* currentType;
+
+class MaterialPool;
+extern MaterialPool* materialPool;
+} // namespace Materials
