dbb9773d75c44ee180759d6efd5d2658708c76b6
johannes <johannes.hirche@gmail.com>
2018-11-06 21:20:09 +0100

    [cmake] - modified add_shaders a bit. can now be used to have shaders in folders other than work. currently kind of hardcoded to vulkan because of the use of includes [dynui] - moved imgui shader to addon directory - added basic support for im3d for rendering immediate mode graphics and maya style gizmos, needs work

---
+++ code/addons/dynui/im3d/im3dcontext.h
@@ -0,0 +1,46 @@
+#pragma once
+//------------------------------------------------------------------------------
+/**
+    @class Im3dContext
+
+    Nebula renderer for Im3d gizmos
+
+    (C) 2018 Individual contributors, see AUTHORS file
+*/
+//------------------------------------------------------------------------------
+
+#include "coregraphics/constantbuffer.h"
+#include "coregraphics/vertexbuffer.h"
+#include "coregraphics/indexbuffer.h"
+#include "graphics/graphicscontext.h"
+#include "input/inputevent.h"
+
+namespace Im3d
+{
+
+class Im3dContext : public Graphics::GraphicsContext
+{
+    _DeclareContext();
+public:
+    /// constructor
+    Im3dContext();
+    /// destructor
+    virtual ~Im3dContext();
+
+    static void Create();
+    static void Discard();
+
+    /// called before frame
+    static void OnBeforeView(const Ptr<Graphics::View>& view, const IndexT frameIndex, const Timing::Time frameTime);
+    /// called when rendering a frame batch
+    static void OnRenderAsPlugin(const IndexT frameIndex, const Timing::Time frameTime, const Util::StringAtom& filter);
+       
+    /// handle event
+    static bool HandleInput(const Input::InputEvent& event);
+
+    /// configure size and thickness of gizmos
+    static void SetGizmoSize(int size, int width);
+};
+
+
+} // namespace Im3d
