45f6b30089665e289d414a7eb930ed66f9b69d2b
johannes <johannes.hirche@gmail.com>
2018-11-08 13:59:33 +0100

    [dynui] - fixed wrong line rendering (fixes wrong AA as well) - added grid to im3dcontext

--- code/addons/dynui/im3d/im3dcontext.h
+++ code/addons/dynui/im3d/im3dcontext.h
@@ -34,10 +34,16 @@ public:
     static void OnBeforeView(const Ptr<Graphics::View>& view, const IndexT frameIndex, const Timing::Time frameTime);
     /// called when rendering a frame batch
     static void OnRenderAsPlugin(const IndexT frameIndex, const Timing::Time frameTime, const Util::StringAtom& filter);
-       
+           
     /// handle event
     static bool HandleInput(const Input::InputEvent& event);
 
+    ///
+    static void SetGridStatus(bool enable);
+    ///
+    static void SetGridSize(float cellSize, int cellCount);
+    ///
+    static void SetGridColor(Math::float4 const& color);
     /// configure size and thickness of gizmos
     static void SetGizmoSize(int size, int width);
 };
