0800b42d15402ee7563b4270a4b6c7d326b2088f
johannes <xwolf@ikplus.de>
2019-06-25 10:16:29 +0200

    - started making materials based on json instead of xml/bxml

--- code/render/materials/materialserver.cc
+++ code/render/materials/materialserver.cc
@@ -8,7 +8,7 @@
 #include "surfacepool.h"
 #include "materialtype.h"
 #include "io/ioserver.h"
-#include "io/bxmlreader.h"
+#include "io/jsonreader.h"
 #include "coregraphics/batchgroup.h"
 #include "coregraphics/shaderserver.h"
 
@@ -44,13 +44,13 @@ MaterialServer::Open()
 	this->isOpen = true;
 
 	// load base materials first
-	this->LoadMaterialTypes("mat:base.xml");
+	this->LoadMaterialTypes("mat:base.json");
 
 	// okay, now load the rest of the materials
-	Util::Array<Util::String> materialTables = IO::IoServer::Instance()->ListFiles("mat:", "*.xml");
+	Util::Array<Util::String> materialTables = IO::IoServer::Instance()->ListFiles("mat:", "*.json");
 	for (IndexT i = 0; i < materialTables.Size(); i++)
 	{
-		if (materialTables[i] == "base.xml") continue;
+		if (materialTables[i] == "base.json") continue;
 		this->LoadMaterialTypes("mat:" + materialTables[i]);
 	}
 }
@@ -71,7 +71,7 @@ bool
 MaterialServer::LoadMaterialTypes(const IO::URI& file)
 {
 	Ptr<IO::Stream> stream = IO::IoServer::Instance()->CreateStream(file);
-	Ptr<IO::BXmlReader> reader = IO::BXmlReader::Create();
+	Ptr<IO::JsonReader> reader = IO::JsonReader::Create();
 	reader->SetStream(stream);
 
 	if (reader->Open())
@@ -85,7 +85,7 @@ MaterialServer::LoadMaterialTypes(const IO::URI& file)
 		reader->SetToNode("/Nebula/Materials");
 
 		// parse materials
-		if (reader->SetToFirstChild("Material")) do
+		if (reader->SetToFirstChild()) do
 		{
 			MaterialType* type = this->surfaceAllocator.Alloc<MaterialType>();
 			this->materialTypes.Append(type);
@@ -115,7 +115,7 @@ MaterialServer::LoadMaterialTypes(const IO::URI& file)
 
 			Util::String inherits = reader->GetOptString("inherits", "");
 
-			// load inherited material
+			// load inherited materialx
 			if (!inherits.IsEmpty())
 			{
 				Util::Array<Util::String> inheritances = inherits.Tokenize("|");
@@ -162,141 +162,150 @@ MaterialServer::LoadMaterialTypes(const IO::URI& file)
 
 			// parse passes
 			uint passIndex = 0;
-			if (reader->SetToFirstChild("Pass")) do
+			if (reader->SetToFirstChild("Pass"))
 			{
-				// get batch name
-				Util::String batchName = reader->GetString("batch");
-				Util::String shaderFeatures = reader->GetString("variation");
-
-				// convert batch name to model node type
-				CoreGraphics::BatchGroup::Code code = CoreGraphics::BatchGroup::FromName(batchName);
-
-				//get shader
-				Util::String shaderName = reader->GetString("shader");
-				Resources::ResourceName shaderResId = Resources::ResourceName("shd:" + shaderName + ".fxb");
-				CoreGraphics::ShaderId shd = CoreGraphics::ShaderServer::Instance()->GetShader(shaderResId);
-				CoreGraphics::ShaderFeature::Mask mask = CoreGraphics::ShaderServer::Instance()->FeatureStringToMask(shaderFeatures);
-				CoreGraphics::ShaderProgramId program = CoreGraphics::ShaderGetProgram(shd, mask);
-
-				if (program == CoreGraphics::ShaderProgramId::Invalid())
+				if (reader->SetToFirstChild()) do
 				{
-					n_warning("WARNING: Material '%s' failed to load program with features '%s'\n", type->name.AsCharPtr(), shaderFeatures.AsCharPtr());
-				}
-				else
-				{
-					n_assert(!type->batchToIndexMap.Contains(code));
-					type->batchToIndexMap.Add(code, passIndex++);
-					type->programs.Append(program);
-					type->texturesByBatch.Append({});
-					type->constantsByBatch.Append({});
+					// get batch name
+					Util::String batchName = reader->GetString("batch");
+					Util::String shaderFeatures = reader->GetString("variation");
 
-					// add material to server
-					Util::Array<Materials::MaterialType*>& mats = this->materialTypesByBatch.AddUnique(code);
-					mats.Append(type);
-				}
+					// convert batch name to model node type
+					CoreGraphics::BatchGroup::Code code = CoreGraphics::BatchGroup::FromName(batchName);
 
-			} while (reader->SetToNextChild("Pass"));
+					//get shader
+					Util::String shaderName = reader->GetString("shader");
+					Resources::ResourceName shaderResId = Resources::ResourceName("shd:" + shaderName + ".fxb");
+					CoreGraphics::ShaderId shd = CoreGraphics::ShaderServer::Instance()->GetShader(shaderResId);
+					CoreGraphics::ShaderFeature::Mask mask = CoreGraphics::ShaderServer::Instance()->FeatureStringToMask(shaderFeatures);
+					CoreGraphics::ShaderProgramId program = CoreGraphics::ShaderGetProgram(shd, mask);
 
-			// parse parameters
-			if (reader->SetToFirstChild("Param")) do
-			{
-				// parse parameters
-				Util::String name = reader->GetString("name");
-				Util::String ptype = reader->GetString("type");
-				Util::String desc = reader->GetOptString("desc", "");
-				Util::String editType = reader->GetOptString("edit", "raw");
-				bool system = reader->GetOptBool("system", false);
-
-				if (ptype.BeginsWithString("textureHandle"))
-				{
-					MaterialConstant constant;
-					constant.type = Util::Variant::UInt64;
-					auto res = Resources::CreateResource(reader->GetString("defaultValue") + NEBULA_TEXTURE_EXTENSION, "material types", nullptr, nullptr, true);
-					constant.defaultValue = res.HashCode64();
-					constant.min = -1;
-					constant.max = -1;
-
-					constant.system = system;
-					constant.name = name;
-					constant.offset = { UINT_MAX };
-					constant.slot = InvalidIndex;
-					constant.group = InvalidIndex;
-					type->constants.Add(name, constant);
-				}
-				else if (ptype.BeginsWithString("texture"))
-				{
-					MaterialTexture texture;
-					if (ptype == "texture1d") texture.type = CoreGraphics::Texture1D;
-					else if (ptype == "texture2d") texture.type = CoreGraphics::Texture2D;
-					else if (ptype == "texture3d") texture.type = CoreGraphics::Texture3D;
-					else if (ptype == "texturecube") texture.type = CoreGraphics::TextureCube;
-					else if (ptype == "texture1darray") texture.type = CoreGraphics::Texture1DArray;
-					else if (ptype == "texture2darray") texture.type = CoreGraphics::Texture2DArray;
-					else if (ptype == "texture3darray") texture.type = CoreGraphics::Texture3DArray;
-					else if (ptype == "texturecubearray") texture.type = CoreGraphics::TextureCubeArray;
+					if (program == CoreGraphics::ShaderProgramId::Invalid())
+					{
+						n_warning("WARNING: Material '%s' failed to load program with features '%s'\n", type->name.AsCharPtr(), shaderFeatures.AsCharPtr());
+					}
 					else
 					{
-						n_error("Invalid texture type %s\n", ptype.AsCharPtr());
-					}
-					auto res = Resources::CreateResource(reader->GetString("defaultValue") + NEBULA_TEXTURE_EXTENSION, "material types", nullptr, nullptr, true);
-					texture.defaultValue = res.As<CoreGraphics::TextureId>();
-					texture.system = system;
-					texture.name = name;
-					texture.slot = InvalidIndex;
-					type->textures.Add(name, texture);
-				}
-				else
-				{
-					MaterialConstant constant;
-					constant.type = Util::Variant::StringToType(ptype);
-					switch (constant.type)
-					{
-					case Util::Variant::Float:
-						constant.defaultValue.SetFloat(reader->GetOptFloat("defaultValue", 0.0f));
-						constant.min.SetFloat(reader->GetOptFloat("min", 0.0f));
-						constant.max.SetFloat(reader->GetOptFloat("max", 1.0f));
-						break;
-					case Util::Variant::Int:
-						constant.defaultValue.SetInt(reader->GetOptInt("defaultValue", 0));
-						constant.min.SetInt(reader->GetOptInt("min", 0));
-						constant.max.SetInt(reader->GetOptInt("max", 1));
-						break;
-					case Util::Variant::Bool:
-						constant.defaultValue.SetBool(reader->GetOptBool("defaultValue", false));
-						constant.min.SetBool(false);
-						constant.max.SetBool(true);
-						break;
-					case Util::Variant::Float4:
-						constant.defaultValue.SetFloat4(reader->GetOptFloat4("defaultValue", Math::float4(0, 0, 0, 0)));
-						constant.min.SetFloat4(reader->GetOptFloat4("min", Math::float4(0, 0, 0, 0)));
-						constant.max.SetFloat4(reader->GetOptFloat4("max", Math::float4(1, 1, 1, 1)));
-						break;
-					case Util::Variant::Float2:
-						constant.defaultValue.SetFloat2(reader->GetOptFloat2("defaultValue", Math::float2(0, 0)));
-						constant.min.SetFloat2(reader->GetOptFloat2("min", Math::float2(0, 0)));
-						constant.max.SetFloat2(reader->GetOptFloat2("max", Math::float2(1, 1)));
-						break;
-					case Util::Variant::Matrix44:
-						constant.defaultValue.SetMatrix44(reader->GetOptMatrix44("defaultValue", Math::matrix44::identity()));
-						break;
-					default:
-						n_error("Unknown material parameter type %s\n", ptype.AsCharPtr());
-					}
+						n_assert(!type->batchToIndexMap.Contains(code));
+						type->batchToIndexMap.Add(code, passIndex++);
+						type->programs.Append(program);
+						type->texturesByBatch.Append({});
+						type->constantsByBatch.Append({});
 
-					constant.system = system;
-					constant.name = name;
-					constant.offset = { UINT_MAX };
-					constant.slot = InvalidIndex;
-					constant.group = InvalidIndex;
-					type->constants.Add(name, constant);
-				}
+						// add material to server
+						Util::Array<Materials::MaterialType*>& mats = this->materialTypesByBatch.AddUnique(code);
+						mats.Append(type);
+					}
+				} while (reader->SetToNextChild());
+				reader->SetToParent();
 			}
-			while (reader->SetToNextChild("Param"));
+			
+			// parse parameters
+			if (reader->SetToFirstChild("Param"))
+			{
+				if (reader->SetToFirstChild()) do
+				{
+					// parse parameters
+					Util::String name = reader->GetString("name");
+					Util::String ptype = reader->GetString("type");
+					Util::String desc = reader->GetOptString("desc", "");
+					Util::String editType = reader->GetOptString("edit", "raw");
+					bool system = reader->GetOptBool("system", false);
 
+					if (ptype.BeginsWithString("textureHandle"))
+					{
+						MaterialConstant constant;
+						constant.type = Util::Variant::UInt64;
+						auto res = Resources::CreateResource(reader->GetString("defaultValue") + NEBULA_TEXTURE_EXTENSION, "material types", nullptr, nullptr, true);
+						constant.defaultValue = res.HashCode64();
+						constant.min = -1;
+						constant.max = -1;
+
+						constant.system = system;
+						constant.name = name;
+						constant.offset = { UINT_MAX };
+						constant.slot = InvalidIndex;
+						constant.group = InvalidIndex;
+						type->constants.Add(name, constant);
+					}
+					else if (ptype.BeginsWithString("texture"))
+					{
+						MaterialTexture texture;
+						if (ptype == "texture1d") texture.type = CoreGraphics::Texture1D;
+						else if (ptype == "texture2d") texture.type = CoreGraphics::Texture2D;
+						else if (ptype == "texture3d") texture.type = CoreGraphics::Texture3D;
+						else if (ptype == "texturecube") texture.type = CoreGraphics::TextureCube;
+						else if (ptype == "texture1darray") texture.type = CoreGraphics::Texture1DArray;
+						else if (ptype == "texture2darray") texture.type = CoreGraphics::Texture2DArray;
+						else if (ptype == "texture3darray") texture.type = CoreGraphics::Texture3DArray;
+						else if (ptype == "texturecubearray") texture.type = CoreGraphics::TextureCubeArray;
+						else
+						{
+							n_error("Invalid texture type %s\n", ptype.AsCharPtr());
+						}
+						auto res = Resources::CreateResource(reader->GetString("defaultValue") + NEBULA_TEXTURE_EXTENSION, "material types", nullptr, nullptr, true);
+						texture.defaultValue = res.As<CoreGraphics::TextureId>();
+						texture.system = system;
+						texture.name = name;
+						texture.slot = InvalidIndex;
+						type->textures.Add(name, texture);
+					}
+					else
+					{
+						MaterialConstant constant;
+						constant.type = Util::Variant::StringToType(ptype);
+						switch (constant.type)
+						{
+						case Util::Variant::Float:
+							constant.defaultValue.SetFloat(reader->GetOptFloat("defaultValue", 0.0f));
+							constant.min.SetFloat(reader->GetOptFloat("min", 0.0f));
+							constant.max.SetFloat(reader->GetOptFloat("max", 1.0f));
+							break;
+						case Util::Variant::Int:
+							constant.defaultValue.SetInt(reader->GetOptInt("defaultValue", 0));
+							constant.min.SetInt(reader->GetOptInt("min", 0));
+							constant.max.SetInt(reader->GetOptInt("max", 1));
+							break;
+						case Util::Variant::Bool:
+							constant.defaultValue.SetBool(reader->GetOptBool("defaultValue", false));
+							constant.min.SetBool(false);
+							constant.max.SetBool(true);
+							break;
+						case Util::Variant::Float4:
+							constant.defaultValue.SetFloat4(reader->GetOptFloat4("defaultValue", Math::float4(0, 0, 0, 0)));
+							constant.min.SetFloat4(reader->GetOptFloat4("min", Math::float4(0, 0, 0, 0)));
+							constant.max.SetFloat4(reader->GetOptFloat4("max", Math::float4(1, 1, 1, 1)));
+							break;
+						case Util::Variant::Float2:
+							constant.defaultValue.SetFloat2(reader->GetOptFloat2("defaultValue", Math::float2(0, 0)));
+							constant.min.SetFloat2(reader->GetOptFloat2("min", Math::float2(0, 0)));
+							constant.max.SetFloat2(reader->GetOptFloat2("max", Math::float2(1, 1)));
+							break;
+						case Util::Variant::Matrix44:
+							constant.defaultValue.SetMatrix44(reader->GetOptMatrix44("defaultValue", Math::matrix44::identity()));
+							break;
+						default:
+							n_error("Unknown material parameter type %s\n", ptype.AsCharPtr());
+						}
+
+						constant.system = system;
+						constant.name = name;
+						constant.offset = { UINT_MAX };
+						constant.slot = InvalidIndex;
+						constant.group = InvalidIndex;
+						type->constants.Add(name, constant);
+					}
+				} while (reader->SetToNextChild());
+				reader->SetToParent();
+			}
+			            
 			// setup type (this maps valid constants to their respective programs)
 			type->Setup();
+
+			
+			
 		} 
-		while (reader->SetToNextChild("Material"));
+		while (reader->SetToNextChild());
 
 		return true;
 	}
