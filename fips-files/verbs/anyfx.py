bbfed3b791d7d3ec303d5965ddb8207438584eed
johannes <johannes.hirche@gmail.com>
2019-06-25 22:48:54 +0200

    - added anyfx fips verb

---
+++ fips-files/verbs/anyfx.py
@@ -0,0 +1,34 @@
+"""Build and deploy anyfxcompiler
+setup
+"""
+
+from mod import log, util, settings
+import os
+import sys
+import shutil
+import subprocess
+
+def run(fips_dir, proj_dir, args) :
+    """run the 'anyfx' verb"""
+    if sys.platform == "win32" :
+        target = "anyfxcompiler-windows/anyfxcompiler.exe"        
+    else :
+        target = "anyfxcompiler-linux/anyfxcompiler"        
+    if len(args) > 0 :
+        noun = args[0]
+        if noun == 'setup' :
+            target = util.fix_path(proj_dir + "/../fips-deploy/fips-anyfx/" + target)
+            if not os.path.isfile(target) :
+                log.info(log.YELLOW + "Compiling anyfxcompiler")
+                if sys.playform == "win32" :
+                    subprocess.call(proj_dir + "/../fips-anyfx/anyfxcompiler/build.bat")
+                else :
+                    subprocess.call(proj_dir + "/../fips-anyfx/anyfxcompiler/build.sh")
+            else :
+                log.info(log.YELLOW + "anyfxcompiler already built, skipping")
+
+def help():
+    """print 'anyfx' help"""
+    log.info(log.YELLOW +
+             "fips anyfx setup\n"
+             "  compiles anyfxcompiler for platform\n")
