4d13d4477bf74b35209016a458ff68049f882ed2
johannes <johannes.hirche@gmail.com>
2019-06-26 15:42:04 +0200

    - remove workaround for double fips imports

--- fips-files/verbs/anyfx.py
+++ fips-files/verbs/anyfx.py
@@ -23,9 +23,7 @@ def run(fips_dir, proj_dir, args) :
                 if sys.platform == "win32" :
                     subprocess.call(proj_dir + "/../fips-anyfx/anyfxcompiler/build.bat")
                 else :
-                    subprocess.call(proj_dir + "/../fips-anyfx/anyfxcompiler/build.sh")
-                # remove fips import file as it causes double cmake targets at the moment
-                os.remove(proj_dir + "/../fips-anyfx/.fips-imports.cmake")
+                    subprocess.call(proj_dir + "/../fips-anyfx/anyfxcompiler/build.sh")                
             else :
                 log.info(log.YELLOW + "anyfxcompiler already built, skipping")
 
