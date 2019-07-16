0998bb4be2658ad028a518faff63c0c34e424b68
johannes <johannes.hirche@gmail.com>
2019-07-16 15:57:28 +0200

    - reenabled filewatchertest

--- testfoundation/filewatchertest.cc
+++ testfoundation/filewatchertest.cc
@@ -28,44 +28,75 @@ FileWatcherTest::Run()
 {
     Ptr<IoServer> ioServer = IoServer::Create();
     AssignRegistry* assignRegistry = AssignRegistry::Instance();
-    Ptr<FileWatcher> watcher = FileWatcher::Create();
+    
 
     ioServer->CreateDirectory("temp");
-    
-    bool fileAdded = false;
-    bool fileModified = false;
-    bool fileDeleted = false;
-    watcher->Watch("temp", false, WatchFlags(WatchFlags::Creation | WatchFlags::Write), [&fileAdded,&fileModified,&fileDeleted](IO::WatchEvent const& ev)
+    ioServer->CreateDirectory("temp2");
+        
+    int fileAdded = 0;
+    int fileModified = 0;
+    int fileDeleted = 0;
+    Ptr<FileWatcher> watcher = FileWatcher::Create();
+    watcher->Setup();
+    watcher->SetSpeed(0.001);
+    watcher->Watch("temp", false, WatchFlags(WatchFlags::Creation | WatchFlags::NameChanged | WatchFlags::Write), [&fileAdded, &fileModified, &fileDeleted](IO::WatchEvent const& ev)
     {
+        n_printf("event temp: %d %s\n", ev.type, ev.file.AsCharPtr());
         switch(ev.type)
-        {
-        case IO::Created: fileAdded = true; break;
-        case IO::Modified: fileModified = true; break;
-        case IO::Deleted: fileDeleted = true; break;
+        {        
+        case IO::Created: fileAdded++; break;
+        case IO::Modified: fileModified++ ; break;
+        case IO::Deleted: fileDeleted++; break;
         }        
     });
+    watcher->Watch("temp2", false, WatchFlags(WatchFlags::Creation | WatchFlags::NameChanged | WatchFlags::Write), [&fileAdded, &fileModified, &fileDeleted](IO::WatchEvent const& ev)
+    {
+        n_printf("event temp2: %d %s\n", ev.type, ev.file.AsCharPtr());
+        switch (ev.type)
+        {
+        case IO::Created: fileAdded++; break;
+        case IO::Modified: fileModified++; break;
+        case IO::Deleted: fileDeleted++; break;
+        }
+    });
+    Core::SysFunc::Sleep(0.2);
     Ptr<Stream> stream = ioServer->Instance()->CreateStream("temp/test.txt");
     Ptr<TextWriter> writer = TextWriter::Create();
     writer->SetStream(stream);
     writer->Open();
     writer->WriteLine("Testline");
-    writer->Close();
-    watcher->Update();
-    watcher->Update();
-    watcher->Update();
-    VERIFY(fileAdded == true);
+    writer->Close();        
+    stream = nullptr;
+    
     stream = ioServer->Instance()->CreateStream("temp/test.txt");    
     writer->SetStream(stream);
     writer->Open();
     writer->WriteLine("Testline2");
-    writer->Close();
-    watcher->Update();
-    VERIFY(fileModified == true);
-    ioServer->Instance()->DeleteFile("temp/test.txt");
-    watcher->Update();
-    watcher->Update();
-    VERIFY(fileDeleted == true);
+    writer->Close();    
+    stream = nullptr;
+
+    ioServer->Instance()->CopyFile("temp/test.txt", "temp2/test.txt");
+   
+    ioServer->Instance()->DeleteFile("temp/test.txt");  
+
+    
+    ioServer->Instance()->DeleteFile("temp2/test.txt");
+    
+    
+    // loop for a while to give thread time to trigger events
+    for (int i = 0; i < 100; i++)
+    {
+        if (fileDeleted == 2) break;
+        Core::SysFunc::Sleep(0.1);
+    }
+
+    watcher->Unwatch("temp2");
     watcher->Unwatch("temp");
-    ioServer->Instance()->DeleteDirectory("temp");
+    ioServer->Instance()->DeleteDirectory("temp");       
+    ioServer->Instance()->DeleteDirectory("temp2");
+        
+    VERIFY(fileAdded == 2);
+    VERIFY(fileModified == 4);
+    VERIFY(fileDeleted == 2);
 }
 }
