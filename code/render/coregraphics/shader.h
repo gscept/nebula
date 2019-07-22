8bc29a335be478b07cd318498f58f7563688fc14
Gustav Sterbrant <gustav.sterbrant@gmail.com>
2019-07-22 14:36:24 +0200

    [render] Updated shader with the new constant buffer flags.

--- code/render/coregraphics/shader.h
+++ code/render/coregraphics/shader.h
@@ -94,9 +94,9 @@ const ShaderId ShaderGet(const Resources::ResourceName& name);
 /// create resource table from shader
 const ResourceTableId ShaderCreateResourceTable(const ShaderId id, const IndexT group);
 /// create constant buffer from shader using name (don't use too frequently)
-const ConstantBufferId ShaderCreateConstantBuffer(const ShaderId id, const Util::StringAtom& name, ConstantBufferUpdateMode mode = ConstantBufferUpdateMode::CoherentlyMappedMemory);
+const ConstantBufferId ShaderCreateConstantBuffer(const ShaderId id, const Util::StringAtom& name, ConstantBufferUpdateMode mode = ConstantBufferUpdateMode::HostWriteable);
 /// create constant buffer from index
-const ConstantBufferId ShaderCreateConstantBuffer(const ShaderId id, const IndexT cbIndex, ConstantBufferUpdateMode mode = ConstantBufferUpdateMode::CoherentlyMappedMemory);
+const ConstantBufferId ShaderCreateConstantBuffer(const ShaderId id, const IndexT cbIndex, ConstantBufferUpdateMode mode = ConstantBufferUpdateMode::HostWriteable);
 
 /// get the number of constants from shader
 const SizeT ShaderGetConstantCount(const ShaderId id);
