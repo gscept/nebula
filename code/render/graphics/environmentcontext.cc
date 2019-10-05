14175e3f723a5dccbefe7bcd64ec990408cd9417
Gustav Sterbrant <gustav.sterbrant@gmail.com>
2019-10-05 15:28:36 +0200

    Added debug frame buffer inspector, and a stub class for the environment rendering.

---
+++ code/render/graphics/environmentcontext.cc
@@ -0,0 +1,10 @@
+//------------------------------------------------------------------------------
+//  environmentcontext.cc
+//  (C) 2018 Individual contributors, see AUTHORS file
+//------------------------------------------------------------------------------
+#include "stdneb.h"
+#include "environmentcontext.h"
+namespace Graphics
+{
+
+} // namespace Graphics
