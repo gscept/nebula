926e842b621e6278ab94ce8b301ffb6da00e7a77
Gustav Sterbrant <gustav.sterbrant@gmail.com>
2019-10-05 15:22:15 +0200

    Frame subpass applies whole array of viewports and scissor rects instead of one by one.

--- code/render/frame/framesubpass.cc
+++ code/render/frame/framesubpass.cc
@@ -58,15 +58,25 @@ FrameSubpass::CompiledImpl::Run(const IndexT frameIndex)
 {
 	IndexT i;
 
+#if NEBULA_GRAPHICS_DEBUG
+	CoreGraphics::CommandBufferBeginMarker(GraphicsQueueType, NEBULA_MARKER_GREEN, this->name.Value());
+#endif
+
 	// bind scissors and viewports, if any
-	for (i = 0; i < this->viewports.Size(); i++) CoreGraphics::SetViewport(this->viewports[i], i);
-	for (i = 0; i < this->scissors.Size(); i++) CoreGraphics::SetScissorRect(this->scissors[i], i);
+	if (this->viewports.Size() > 0)
+		CoreGraphics::SetViewports(this->viewports.Begin(), this->viewports.Size());
+	if (this->scissors.Size() > 0)
+		CoreGraphics::SetScissorRects(this->scissors.Begin(), this->scissors.Size());
 
 	// run ops
 	for (i = 0; i < this->ops.Size(); i++)
 	{
 		this->ops[i]->Run(frameIndex);
 	}
+
+#if NEBULA_GRAPHICS_DEBUG
+	CoreGraphics::CommandBufferEndMarker(GraphicsQueueType);
+#endif
 }
 
 //------------------------------------------------------------------------------
@@ -88,6 +98,9 @@ FrameSubpass::AllocCompiled(Memory::ArenaAllocator<BIG_CHUNK>& allocator)
 	CompiledImpl* ret = allocator.Alloc<CompiledImpl>();
 	ret->viewports = this->viewports;
 	ret->scissors = this->scissors;
+#if NEBULA_GRAPHICS_DEBUG
+	ret->name = this->name;
+#endif
 	// don't set ops here, we have to do it when we build
 	return ret;
 }
