e698845e90cc786cd32c678a1ca7fb3b02593b41
Fredrik Lindahl <fredrik-lindahl@outlook.com>
2019-10-29 17:09:08 +0100

    [scripting] Fixed bugs with type conversion between python and native types. Added a couple of nebula type definitions in python.

--- code/addons/scripting/python/conversion.h
+++ code/addons/scripting/python/conversion.h
@@ -12,19 +12,62 @@
 #include "math/transform44.h"
 #include "pybind11/embed.h"
 #include "pybind11/detail/class.h"
+#include "pybind11/stl.h"
 
 namespace Python
 {
 
-
 //------------------------------------------------------------------------------
 /**
 */
-Util::Variant ConvertPyType(pybind11::handle const& value, Util::Variant::Type type);
-
-//------------------------------------------------------------------------------
-/**
-*/
-pybind11::handle VariantToPyType(Util::Variant src, pybind11::return_value_policy policy = pybind11::return_value_policy::automatic);
+pybind11::handle VariantToPyType(Util::Variant src, pybind11::return_value_policy policy = pybind11::return_value_policy::automatic, pybind11::handle parent = NULL);
 
 } // namespace Python
+
+namespace pybind11
+{
+    namespace detail
+    {
+        template <> class type_caster<Util::Variant>
+        {
+        public:
+            template<class T>
+			bool try_load(handle src, bool convert) {
+				auto caster = make_caster<T>();
+				if (caster.load(src, convert)) {
+					value = cast_op<T>(caster);
+					return true;
+				}
+				return false;
+			}
+
+			bool load(handle src, bool convert) {
+				auto loaded = {
+					false,
+					try_load<int>(src, convert),
+					try_load<uint>(src, convert),
+					try_load<float>(src, convert),
+					try_load<Math::float4>(src, convert),
+					try_load<Util::String>(src, convert),
+					try_load<Util::Guid>(src, convert),
+					try_load<Util::Array<int>>(src, convert),
+					try_load<Util::Array<float>>(src, convert),
+					try_load<Util::Array<Math::float4>>(src, convert),
+					try_load<Util::Array<Util::String>>(src, convert),
+					try_load<Util::Array<Util::Guid>>(src, convert)
+				};
+				return std::any_of(loaded.begin(), loaded.end(), [](bool b) { return b; });
+			}
+
+            static handle cast(const Util::Variant& src, return_value_policy policy, handle parent) {
+                return Python::VariantToPyType(src, policy, parent);
+            }
+            PYBIND11_TYPE_CASTER(Util::Variant, _("Variant"));
+        };
+
+        template <typename Key, typename Value> struct type_caster<Util::Dictionary<Key, Value>>
+            : map_caster<Util::Dictionary<Key, Value>, Key, Value> { };
+        template <typename Type> struct type_caster<Util::Array<Type>>
+            : array_caster<Util::Array<Type>, Type, true> { };   
+    }
+}
