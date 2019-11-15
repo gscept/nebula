56b6a177aec34b0215f1451bd3156a24aba0f872
Fredrik Lindahl <fredrik-lindahl@outlook.com>
2019-11-15 09:53:26 +0100

    [attr] Fixed build errors

--- code/addons/attr/commonattributes.cc
+++ code/addons/attr/commonattributes.cc
@@ -2,7 +2,7 @@
 //  commonattributes.cc
 //  (C) 2019 Individual contributors, see AUTHORS file
 //------------------------------------------------------------------------------
-#include "stdneb.h"
+#include "foundation/stdneb.h"
 #include "commonattributes.h"
 
 namespace Attr
