27556e836a0a8f62f7c144291c119936df0f6c99
Fredrik Lindahl <fredrik-lindahl@outlook.com>
2020-01-09 14:12:28 +0100

    [render] Fixed performance warning

--- code/render/characters/charactercontext.cc
+++ code/render/characters/charactercontext.cc
@@ -741,7 +741,7 @@ CharacterContext::OnAfterFrame(const IndexT frameIndex, const Timing::Time frame
 	if (CharacterContext::runningJobs.Size() > 0)
 	{
 		Util::Array<Jobs::JobId> jobs;
-		jobs.Reserve(500);
+		jobs.Reserve(CharacterContext::runningJobs.Size());
 		CharacterContext::runningJobs.DequeueAll(jobs);
 
 		// wait for all jobs to finish
