fa572e91d5fba2179ea0148ab9368f7ec8abd7c9
Fredrik Lindahl <fredrik-lindahl@outlook.com>
2020-02-10 15:42:45 +0100

    [game] renamed ColumnReader -> ColumnData renamed ColumnData -> ColumnBuffer

--- code/application/game/database/database.h
+++ code/application/game/database/database.h
@@ -22,27 +22,17 @@ struct TableCreateInfo
 
 
 template<typename TYPE>
-class ColumnReader
+class ColumnData
 {
 public:
-    ColumnReader() : data(nullptr)
+    ColumnData() : data(nullptr)
     {
     }
-    ColumnReader(void** ptrptr) : data((void**)ptrptr)
+    ColumnData(void** ptrptr) : data((void**)ptrptr)
     {
     }
-    //PropertyData(PropertyData const& rhs) : data(rhs.data) {}
-    /*PropertyData(PropertyData&& rhs)
-    {
-        data = rhs.data;
-        rhs.data = nullptr;
-    }
-    void operator=(PropertyData const& rhs)
-    {
-        this->data = rhs.data;
-    }*/
-
-    ~ColumnReader() = default;
+    
+    ~ColumnData() = default;
     TYPE& operator[](IndexT index)
     {
         n_assert(this->data != nullptr);
@@ -80,11 +70,11 @@ public:
     SizeT GetNumRows(TableId table);
     
     template<typename ATTR>
-    Game::ColumnReader<typename ATTR::InnerType> GetColumnData(TableId table)
+    Game::ColumnData<typename ATTR::InnerType> GetColumnData(TableId table)
     {
         Game::Database::Table& tbl = this->tables.Get<0>(Ids::Index(table.id));
         ColumnId cid = this->GetColumnId(table, Attr::AttrId(ATTR::FourCC()));
-        return Game::ColumnReader<ATTR::InnerType>(&tbl.columns.Get<1>(cid.id));
+        return Game::ColumnData<ATTR::InnerType>(&tbl.columns.Get<1>(cid.id));
     }
 
 private:
@@ -94,10 +84,10 @@ private:
     
     struct Table
     {
-        using ColumnData = void*;
+        using ColumnBuffer = void*;
 
         Util::StringAtom name;
-        Util::ArrayAllocator<Column, ColumnData> columns;
+        Util::ArrayAllocator<Column, ColumnBuffer> columns;
         uint32_t numRows = 0;
         uint32_t capacity = 128;
         uint32_t grow = 128;
