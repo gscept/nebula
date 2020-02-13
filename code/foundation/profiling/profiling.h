cc848728398cdaa46aabbc463c61dfe93673536d
Gustav Sterbrant <gustav.sterbrant@gmail.com>
2020-02-13 14:51:19 +0100

    [foundation] Added accumulative profiling scopes for cases where it's hard to put a single big scope to cover executions of the same thing, use with caution as it will create just one big contiguous marker.

--- code/foundation/profiling/profiling.h
+++ code/foundation/profiling/profiling.h
@@ -22,8 +22,10 @@
 #if NEBULA_ENABLE_PROFILING
 #define N_SCOPE(name, cat)              Profiling::ProfilingScopeLock __##name##cat##scope__(#name, #cat, __FILE__, __LINE__);
 #define N_SCOPE_DYN(str, cat)           Profiling::ProfilingScopeLock __dynscope##cat##__(str, #cat, __FILE__, __LINE__);
-#define N_MARKER_BEGIN(name, cat)       { Profiling::ProfilingScope __##name##cat##scope__ = {#name, #cat, __FILE__, __LINE__}; Profiling::ProfilingPushScope(__##name##cat##scope__); }
-#define N_MARKER_DYN_BEGIN(str, cat)    { Profiling::ProfilingScope __dynmarker##cat##__ = {str, #cat, __FILE__, __LINE__}; Profiling::ProfilingPushScope(__dynmarker##cat##__); }
+#define N_SCOPE_ACCUM(name, cat)        Profiling::ProfilingScopeLock __##name##cat##scope__(#name, #cat, __FILE__, __LINE__, true);
+#define N_SCOPE_DYN_ACCUM(name, cat)    Profiling::ProfilingScopeLock __##name##cat##scope__(#name, #cat, __FILE__, __LINE__, true);
+#define N_MARKER_BEGIN(name, cat)       { Profiling::ProfilingScope __##name##cat##scope__ = {#name, #cat, __FILE__, __LINE__, false}; Profiling::ProfilingPushScope(__##name##cat##scope__); }
+#define N_MARKER_DYN_BEGIN(str, cat)    { Profiling::ProfilingScope __dynmarker##cat##__ = {str, #cat, __FILE__, __LINE__, false}; Profiling::ProfilingPushScope(__dynmarker##cat##__); }
 #define N_MARKER_END()                  { Profiling::ProfilingPopScope(); }
 #else
 #define N_SCOPE(name, cat)
@@ -72,22 +74,25 @@ struct ProfilingScope
         , line(-1)
         , duration(0)
         , start(0)
+        , accum(false)
     {};
 
     /// constructor
-    ProfilingScope(const char* name, Util::StringAtom category, const char* file, int line)
+    ProfilingScope(const char* name, Util::StringAtom category, const char* file, int line, bool accum)
         : name(name)
         , category(category)
         , file(file)
         , line(line)
         , duration(0)
         , start(0)
+        , accum(accum)
     {};
 
     const char* name;
     Util::StringAtom category;
     const char* file;
     int line;
+    bool accum;
 
     Timing::Time start;
     Timing::Time duration;
@@ -98,7 +103,7 @@ struct ProfilingScope
 struct ProfilingScopeLock
 {
     /// constructor
-    ProfilingScopeLock(const char* name, Util::StringAtom category, const char* file, int line)
+    ProfilingScopeLock(const char* name, Util::StringAtom category, const char* file, int line, bool accum = false)
     {
         scope.name = name;
         scope.category = category;
