97ecad028e243b820f8e90e7537fd3c56abaff58
Fredrik Lindahl <fredrik-lindahl@outlook.com>
2020-03-05 22:30:50 +0100

    [game] Reimplemented physics feature unit. Added rigidbody property.

---
+++ code/application/physicsfeature/properties/rigidbodyproperty.cc
@@ -0,0 +1,86 @@
+//------------------------------------------------------------------------------
+//  physicscomponent.cc
+//  (C) 2019-2020 Individual contributors, see AUTHORS file
+//------------------------------------------------------------------------------
+#include "application/stdneb.h"
+#include "rigidbodyproperty.h"
+#include "physicsinterface.h"
+#include "physics/streamactorpool.h"
+#include "physics/actorcontext.h"
+#include "resources/resourcemanager.h"
+
+namespace PhysicsFeature
+{
+
+__ImplementClass(PhysicsFeature::RigidBodyProperty, 'PRBP', Game::Property);
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+RigidBodyProperty::MoveCallback(Physics::ActorId id, Math::matrix44 const& trans)
+{
+    Physics::Actor& actor = Physics::ActorContext::GetActor(id);
+    Game::InstanceId instance = (Game::InstanceId)actor.userData;
+	this->data.worldTransform[instance.id] = trans;
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+RigidBodyProperty::Init()
+{
+	this->data = {
+		Game::CreatePropertyState<RigidBodyState>(this->category),
+		Game::GetPropertyData<Attr::WorldTransform>(this->category),
+		Game::GetPropertyData<Attr::IsDynamic>(this->category),
+		Game::GetPropertyData<Attr::PhysicsResource>(this->category),
+	};
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+RigidBodyProperty::OnActivate(Game::InstanceId instance)
+{
+	auto trans = this->data.worldTransform[instance.id];
+	auto dynamic = this->data.isDynamic[instance.id];
+	auto& actorid = this->data.state[instance.id].actorid;
+	auto const& resource = this->data.resource[instance.id];
+    this->data.state[instance.id].actorResource = Resources::CreateResource(this->data.resource[instance.id],"NONE",
+        [this, trans, dynamic, &actorid, instance](Resources::ResourceId id)
+        {
+            actorid = Physics::CreateActorInstance(id, trans, dynamic);
+            Physics::Actor& actor = Physics::ActorContext::GetActor(actorid);
+            actor.userData = instance.id;
+            actor.moveCallback = Util::Delegate<void(Physics::ActorId, Math::matrix44 const&)>::FromMethod<RigidBodyProperty, MoveCallback>(this);
+        },[&resource, instance](Resources::ResourceId id) 
+        {
+            n_warning("failed to load physics actor from %s\n", resource);
+        }, true);
+
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+RigidBodyProperty::OnDeactivate(Game::InstanceId instance)
+{
+    // FIXME	
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+RigidBodyProperty::SetWorldTransform(Game::Entity entity, const Math::matrix44& val)
+{
+	//n_assert(Game::GetEntityCategory(entity) == this->category);
+	Physics::ActorId actorId = this->data.state[Game::GetInstanceId(entity).id].actorid;
+    Physics::ActorContext::SetTransform(actorId, val);
+}
+
+} // namespace PhysicsFeature
---
+++ code/application/physicsfeature/properties/rigidbodyproperty.h
@@ -0,0 +1,50 @@
+#pragma once
+//------------------------------------------------------------------------------
+/**
+	PhysicsComponents
+
+	(C) 2019-2020 Individual contributors, see AUTHORS file
+*/
+//------------------------------------------------------------------------------
+#include "game/property.h"
+#include "game/database/attribute.h"
+#include "basegamefeature/properties/transformableproperty.h"
+#include "physicsinterface.h"
+
+namespace Attr
+{
+    __DeclareAttribute(IsDynamic, bool, 'phDy', bool(true));
+    __DeclareAttribute(PhysicsResource, Util::String, 'phRs', Util::String(""));
+}
+
+namespace PhysicsFeature
+{
+
+class RigidBodyProperty : public Game::Property
+{
+    __DeclareClass(RigidBodyProperty)
+public:
+    void Init() override;
+
+    void OnActivate(Game::InstanceId instance) override;
+    void OnDeactivate(Game::InstanceId instance) override;
+
+    void SetWorldTransform(Game::Entity entity, const Math::matrix44& val);
+    void MoveCallback(Physics::ActorId id, Math::matrix44 const& trans);
+
+    struct RigidBodyState
+    {
+        Physics::ActorId actorid;
+        Resources::ResourceId actorResource;
+    };
+private:
+    struct Data
+    {
+        Game::PropertyData<RigidBodyState> state;
+        Game::PropertyData<Math::matrix44> worldTransform;
+        Game::PropertyData<bool> isDynamic;
+        Game::PropertyData<Util::String> resource;
+    } data;
+};
+
+} // namespace PhysicsFeature
