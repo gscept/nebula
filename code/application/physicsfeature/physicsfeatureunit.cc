a0b8bd02bd21f08c1149e4266e5edd04678d4d81
Fredrik Lindahl <fredrik-lindahl@outlook.com>
2020-03-05 22:30:50 +0100

    [game] Reimplemented physics feature unit. Added rigidbody property.

---
+++ code/application/physicsfeature/physicsfeatureunit.cc
@@ -0,0 +1,73 @@
+//------------------------------------------------------------------------------
+//  physicsfeature/physicsfeatureunit.cc
+//  (C) 2019-2020 Individual contributors, see AUTHORS file
+//------------------------------------------------------------------------------
+#include "application/stdneb.h"
+#include "Physicsfeature/Physicsfeatureunit.h"
+#include "physicsinterface.h"
+#include "graphics/graphicsserver.h"
+
+namespace PhysicsFeature
+{
+__ImplementClass(PhysicsFeature::PhysicsFeatureUnit, 'PXFU' , Game::FeatureUnit);
+__ImplementSingleton(PhysicsFeatureUnit);
+
+//------------------------------------------------------------------------------
+/**
+*/
+PhysicsFeatureUnit::PhysicsFeatureUnit()
+{
+    __ConstructSingleton;
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+PhysicsFeatureUnit::~PhysicsFeatureUnit()
+{
+    __DestructSingleton;
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+PhysicsFeatureUnit::OnActivate()
+{
+	FeatureUnit::OnActivate();
+    Physics::Setup();
+    Physics::CreateScene();
+	//FIXME
+    IndexT dummyMaterial = Physics::CreateMaterial("dummy"_atm, 0.8, 0.6, 0.3, 1.0);
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+PhysicsFeatureUnit::OnDeactivate()
+{	
+    FeatureUnit::OnDeactivate();    
+    Physics::ShutDown();
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void 
+PhysicsFeatureUnit::OnBeginFrame()
+{
+    //FIXME use own timer or add to basegamefeatureunit
+    Physics::Update(Graphics::GraphicsServer::Instance()->GetFrameTime());
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void 
+PhysicsFeatureUnit::OnRenderDebug()
+{
+    Physics::RenderDebug();
+}
+
+} // namespace Game
