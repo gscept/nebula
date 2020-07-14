82d3bcad80ffe97d2f8a34c8d61ba6abed6f935e
Fredrik Lindahl <fredrik-lindahl@outlook.com>
2020-07-14 20:27:14 +0200

    [game] wip transform manager

--- code/application/basegamefeature/managers/transformmanager.cc
+++ code/application/basegamefeature/managers/transformmanager.cc
@@ -9,5 +9,30 @@
 namespace Game
 {
 
+void /*TransformManager::*/Create()
+{
+	Db::StateDescriptor localTransformDescriptor = Game::EntityManager::Instance()->state.worldDatabase->GetStateDescriptor("LocalTransform"_atm);
+	Db::StateDescriptor worldTransformDescriptor = Game::EntityManager::Instance()->state.worldDatabase->GetStateDescriptor("WorldTransform"_atm);
+
+	/*
+	Game::ProcessorCreateInfo info;
+
+	// can instance specific functions be bundled per category perhaps? (similar to how properties were)
+	// this would save a lot of lookups...
+	info.functions.OnActivateInstance = &OnActivateInstance;
+
+	info.filter.inclusive =	{ localTransformDescriptor, worldTransformDescriptor }
+	info.filter.access =	{ AccessMode::Read,			AccessMode::ReadWrite	 }
+	info.filter.exclusive =	{};
+
+	(global) ProcessorId pid = Game::EntityManager::CreateProcessor(info);
+	*/
+}
+
+void OnActivateInstance(Game::Entity entity, Game::CategoryId category, Game::InstanceId instance, Game::Dataset const* dataset)
+{
+
+	((Math::matrix44*)(dataset->categories[category.id].buffers[0]))[instance.id] = Math::matrix44::identity();
+}
 
 } // namespace Game
