3e949ec92fd19f1bb5695fc299cc6169f04ff511
Fredrik Lindahl <fredrik-lindahl@outlook.com>
2020-10-22 10:43:41 +0200

    [foundation] Bitfields are now trivially copyable and assignable Added tests for bitfields Added static assertion to assure bitfield num bits is greater than zero

---
+++ tests/testfoundation/bitfieldtest.h
@@ -0,0 +1,25 @@
+#pragma once
+//------------------------------------------------------------------------------
+/**
+    @class Test::BlobTest
+    
+    Test BitField functionality
+    
+    (C) 2018 Individual contributors, see AUTHORS file
+*/
+#include "testbase/testcase.h"
+
+//------------------------------------------------------------------------------
+namespace Test
+{
+class BitFieldTest : public TestCase
+{
+    __DeclareClass(BitFieldTest);
+public:
+    /// run the test
+    virtual void Run();
+};
+
+}; // namespace Test
+//------------------------------------------------------------------------------
+    
