b5f6292d9ca236e7a04f3ed746f882423d0a7bf8
Gustav Sterbrant <gustav.sterbrant@gmail.com>
2020-11-27 23:35:28 +0100

    Added a new job system
    Using fibers as the driving mechanism, the fiber queue allows for jobs which are more like ordinary functions to execute. This functions can also invoke new jobs, and subsequently wait for them, allowing us to have much more complicated execution paths on threads, and more efficiently fill our threads with work.

---
+++ code/foundation/fibers/fibers.cc
@@ -0,0 +1,270 @@
+//------------------------------------------------------------------------------
+//  jobs.cc
+//  (C) 2020 Individual contributors, see AUTHORS file
+//------------------------------------------------------------------------------
+#include "foundation/stdneb.h"
+#include "system/cpu.h"
+#include "profiling/profiling.h"
+#include "fibers.h"
+namespace Fibers
+{
+
+thread_local FiberThread* currentThread;
+
+__ImplementClass(Fibers::FiberThread, 'FITH', Threading::Thread);
+//------------------------------------------------------------------------------
+/**
+*/
+FiberThread::FiberThread()
+{
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+FiberThread::~FiberThread()
+{
+
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+FiberThread::EmitWakeupSignal()
+{
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+FiberThread::DoWork()
+{
+#if NEBULA_ENABLE_PROFILING
+	Profiling::ProfilingRegisterThread();
+#endif
+
+	// convert thread to fiber
+	Fibers::Fiber::ThreadToFiber(this->threadFiber);
+
+	currentThread = this;
+	while (!this->ThreadStopRequested())
+	{
+		NewFiber();
+	}
+
+	// convert back to thread and destroy fiber
+	Fibers::Fiber::FiberToThread(this->threadFiber);
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+bool
+FiberThread::HasWork()
+{
+	return false;
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+FiberThread::SleepFiber(AtomicCounter* counter, int value)
+{
+	FiberQueue::Sleep(FiberWaitContext{ this->currentFiber.fiber, counter, value });
+
+	// go back to main loop
+	this->SwitchFiber();
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+FiberThread::NewFiber()
+{
+	// wakeup waiting fibers
+	if (!FiberQueue::WakeupFiber(this->currentFiber))
+	{
+		// if not, try dequeue pending fiber
+		if (!FiberQueue::Dequeue(this->currentFiber))
+		{
+			// if all fails, return
+			return;
+		}
+	}
+
+	// set the current fiber, start it and when finished, decrement counter
+	this->currentFiber.fiber->Start();
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+FiberThread::SwitchFiber()
+{
+	this->threadFiber.Start();
+}
+
+Threading::LockFreeQueue<FiberQueue::Job> FiberQueue::PendingJobsQueue;
+Threading::LockFreeQueue<uint> FiberQueue::FiberIdQueue;
+Util::FixedArray<Fibers::Fiber> FiberQueue::Fibers;
+Util::FixedArray<FiberQueue::Job> FiberQueue::FiberContexts;
+
+Threading::CriticalSection FiberQueue::SleepLock;
+Util::Array<FiberWaitContext> FiberQueue::SleepingFibers;
+Util::FixedArray<Ptr<FiberThread>> FiberQueue::Threads;
+
+//------------------------------------------------------------------------------
+/**
+*/
+FiberQueue::FiberQueue()
+{
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+FiberQueue::~FiberQueue()
+{
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+FiberQueue::Setup(const FiberQueueCreateInfo& info)
+{
+	FiberQueue::Threads.Resize(info.numThreads);
+	for (uint i = 0; i < info.numThreads; i++)
+	{
+		FiberQueue::Threads[i] = Fibers::FiberThread::Create();
+		FiberQueue::Threads[i]->SetName(Util::String::Sprintf("%s #%d", "Fiber Thread", i));
+		FiberQueue::Threads[i]->SetThreadAffinity(System::Cpu::Core0 << i);
+		FiberQueue::Threads[i]->Start();
+	}
+	FiberQueue::Fibers.Resize(info.numFibers);
+	FiberQueue::FiberContexts.Resize(info.numFibers);
+	FiberQueue::PendingJobsQueue.Resize(65535);
+	FiberQueue::FiberIdQueue.Resize(info.numFibers);
+	for (int i = info.numFibers - 1; i >= 0; i--)
+		FiberQueue::FiberIdQueue.Enqueue(i);
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+FiberQueue::Discard()
+{
+	for (Ptr<FiberThread>& thread : FiberQueue::Threads)
+	{
+		thread->Stop();
+	}
+	FiberQueue::Threads.Clear();
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+FiberQueue::Free(uint id)
+{
+	FiberQueue::FiberIdQueue.Enqueue(id);
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+FiberQueue::Sleep(FiberWaitContext context)
+{
+	Threading::CriticalScope lock(&FiberQueue::SleepLock);
+	FiberQueue::SleepingFibers.Append(context);
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+bool
+FiberQueue::WakeupFiber(Fibers::FiberContext& fiber)
+{
+	// go through fibers and check their wait condition
+	Threading::CriticalScope lock(&FiberQueue::SleepLock);
+	for (IndexT i = 0; i < FiberQueue::SleepingFibers.Size(); i++)
+	{
+		const FiberWaitContext& waitFiber = FiberQueue::SleepingFibers[i];
+
+		// fiber has it's wait condition satisfied, return it
+		if (*waitFiber.counter == waitFiber.value)
+		{
+			fiber.fiber = waitFiber.fiber;
+			fiber.counter = waitFiber.counter;
+			FiberQueue::SleepingFibers.EraseIndex(i);
+			return true;
+		}
+	}
+	return false;
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+FiberFunction(void* param)
+{
+	// get the job information
+	FiberQueue::Job* job = (FiberQueue::Job*)param;
+
+	// run the job function with the job context
+	job->function(job->context);
+
+	// decrement the counter
+	Threading::Interlocked::Decrement(*job->counter);
+	n_assert(*job->counter >= 0);
+
+	// free up context
+	FiberQueue::Free(job->id);
+
+	// continue thread work
+	currentThread->SwitchFiber();
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+bool
+FiberQueue::Dequeue(Fibers::FiberContext& fiber)
+{
+	Job job;
+	uint id;
+
+	// if there are free jobs to pick up, do it
+	if (FiberQueue::PendingJobsQueue.Dequeue(job))
+	{
+		if (FiberQueue::FiberIdQueue.Dequeue(id))
+		{
+			job.id = id;
+
+			// update context and run the constructor on the fiber
+			FiberQueue::FiberContexts[id] = job;
+			new (&FiberQueue::Fibers[id]) Fibers::Fiber{ Fibers::FiberFunction, &FiberQueue::FiberContexts[id] };
+
+			fiber.counter = job.counter;
+			fiber.fiber = &FiberQueue::Fibers[id];
+			return true;
+		}
+		else
+		{
+			// if fiber pool fails, enqueue job again
+			FiberQueue::PendingJobsQueue.Enqueue(job);
+		}
+	}
+
+	return false;
+}
+
+} // namespace Fibers
---
+++ code/foundation/fibers/fibers.h
@@ -0,0 +1,174 @@
+#pragma once
+//------------------------------------------------------------------------------
+/**
+    Fibers are a more lightweight form of jobs which allows recursive job generation, 
+    manual pausing and resuming, and more generic code execution to ordinary jobs.
+
+    (C) 2020 Individual contributors, see AUTHORS file
+*/
+//------------------------------------------------------------------------------
+#include "fiber.h"
+#include "ids/id.h"
+#include "ids/idallocator.h"
+#include "threading/thread.h"
+#include "threading/safequeue.h"
+#include "threading/lockfreequeue.h"
+
+namespace Fibers
+{
+
+struct FiberContext
+{
+    Fibers::Fiber* fiber;
+    volatile int* counter;
+};
+
+struct FiberWaitContext
+{
+    Fibers::Fiber* fiber;
+    volatile int* counter;
+    int value;
+};
+
+class FiberThread : public Threading::Thread
+{
+    __DeclareClass(FiberThread);
+public:
+
+    /// constructor
+    FiberThread();
+    /// destructor
+    virtual ~FiberThread();
+
+    /// called if thread needs a wakeup call before stopping
+    void EmitWakeupSignal();
+    /// this method runs in the thread context
+    void DoWork();
+    /// returns true if thread has work
+    bool HasWork();
+
+    /// sleeps the current fiber and switches it for a new one
+    void SleepFiber(volatile int* counter, int value);
+    /// switches the current fiber
+    void NewFiber();
+    /// switch back to original fiber
+    void SwitchFiber();
+
+private:
+    Fibers::FiberContext currentFiber;
+    Fibers::Fiber threadFiber;
+};
+
+thread_local extern FiberThread* currentThread;
+
+struct FiberQueueCreateInfo
+{
+    uint numThreads;
+    uint numFibers;
+};
+
+typedef volatile int AtomicCounter;
+
+class FiberQueue
+{
+public:
+
+    typedef void (*JobFunction)(void* context);
+    typedef void (*FiberFunction)();
+
+    struct Job
+    {
+        JobFunction function;
+        uint id;
+        void* context;
+        AtomicCounter* counter;
+    };
+
+    /// constructor
+    FiberQueue();
+    /// destructor
+    virtual ~FiberQueue();
+
+    /// setup job queue
+    static void Setup(const FiberQueueCreateInfo& info);
+    /// discard job queue
+    static void Discard();
+    /// enqueue job 
+    template <class T>
+    static void Enqueue(JobFunction function, const Util::FixedArray<T*>& contexts, AtomicCounter* counter);
+    /// free a fiber
+    static void Free(uint id);
+    /// sleep fiber
+    static void Sleep(FiberWaitContext context);
+    /// wake up sleeping fibers
+    static bool WakeupFiber(Fibers::FiberContext& fiber);
+    /// dequeue job as a fiber
+    static bool Dequeue(Fibers::FiberContext& fiber);
+
+private:
+    /// async queues
+    static Threading::LockFreeQueue<Job> PendingJobsQueue;
+    static Threading::LockFreeQueue<uint> FiberIdQueue;
+
+    /// storage
+    static Util::FixedArray<Fibers::Fiber> Fibers;
+    static Util::FixedArray<FiberQueue::Job> FiberContexts;
+    static Util::FixedArray<Ptr<FiberThread>> Threads;
+
+    /// sleeping fibers
+    static Threading::CriticalSection SleepLock;
+    static Util::Array<FiberWaitContext> SleepingFibers;
+};
+
+//------------------------------------------------------------------------------
+/**
+*/
+template <class T>
+inline void
+FiberQueue::Enqueue(JobFunction function, const Util::FixedArray<T*>& contexts, AtomicCounter* counter)
+{
+    Threading::Interlocked::Exchange(counter, contexts.Size());
+
+    for (uint i = 0; i < contexts.Size(); i++)
+    {
+        Job job;
+        job.function = function;
+        job.context = contexts[i];
+        job.counter = counter;
+        FiberQueue::PendingJobsQueue.Enqueue(job);
+        Threading::Interlocked::Increment(FiberQueue::JobsAvailable);
+    }
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+template <class T>
+inline void
+Enqueue(FiberQueue::JobFunction function, const Util::FixedArray<T*>& contexts, AtomicCounter* counter)
+{
+    FiberQueue::Enqueue(function, contexts, counter);
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+inline void 
+Wait(AtomicCounter* counter, int value)
+{
+    if (*counter != value)
+    {
+        currentThread->SleepFiber(counter, value);
+    }
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+inline void
+Lock(AtomicCounter* counter, int value)
+{
+    while (*counter != value) {};
+}
+
+} // namespace Fibers
