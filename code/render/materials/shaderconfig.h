da36f62f49e43fca65469c4665eeda345066ad6c
Gustav <gustav.sterbrant@gmail.com>
2022-01-24 16:39:36 +0000

    [foundation] Added std namespace before nullptr_t.

--- code/render/materials/shaderconfig.h
+++ code/render/materials/shaderconfig.h
@@ -29,7 +29,7 @@ struct ShaderConfigVariant
         , mem(nullptr)
     {};
     /// Nullptr constructor
-    ShaderConfigVariant(nullptr_t)
+    ShaderConfigVariant(std::nullptr_t)
         : type(Type::Invalid)
         , mem(nullptr)
     {}
