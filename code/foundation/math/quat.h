24818bb410f3f0c42da72f5e89108ce60b52aba5
Gustav Sterbrant <gustav.sterbrant@gmail.com>
2023-03-01 21:59:07 +0000

    Changed order of variables to match yaw, pitch, roll instead of pitch, yaw, roll.

--- code/foundation/math/quat.h
+++ code/foundation/math/quat.h
@@ -21,7 +21,7 @@ quat slerp(const quat& q1, const quat& q2, scalar t);
 
 quat rotationmatrix(const mat4& m);
 vec3 to_euler(const quat& q);
-quat quatyawpitchroll(scalar x, scalar y, scalar z);
+quat quatyawpitchroll(scalar y, scalar x, scalar z);
 
 struct NEBULA_ALIGN16 quat
 {
