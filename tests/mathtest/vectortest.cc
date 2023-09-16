b684239e165042c4bf7c1a8685f568c729ece623
Gustav Sterbrant <gustav.sterbrant@gmail.com>
2023-09-16 20:02:44 +0200

    Fix build

--- tests/mathtest/vectortest.cc
+++ tests/mathtest/vectortest.cc
@@ -44,7 +44,7 @@ VectorTest::Run()
     VERIFY(v1 == v3);
     VERIFY(v0 != v1);
     VERIFY(v2 != v3);
-    VERIFY(v0 == vec3(1.0f, 2.0f, 3.0f));
+    VERIFY(vec4equal(v0, vec4(1.0f, 2.0f, 3.0f, 0.0f)));
     // assignemt
     v2 = v1;
     VERIFY(v2 == v1);
