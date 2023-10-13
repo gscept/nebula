2f7274b5b682db6be3b8635797828043cb7aabfd
Gustav Sterbrant <gustav.sterbrant@gmail.com>
2023-10-13 13:55:45 +0200

    Fix image layout transitions with TextureGenerateMipmaps.

--- code/render/coregraphics/texture.cc
+++ code/render/coregraphics/texture.cc
@@ -32,24 +32,9 @@ TextureGenerateMipmaps(const CoreGraphics::CmdBufferId cmdBuf, const TextureId i
     N_CMD_SCOPE(cmdBuf, NEBULA_MARKER_TRANSFER, "Mipmap");
     SizeT numMips = CoreGraphics::TextureGetNumMips(id);
 
-    // insert initial barrier for texture
-    CoreGraphics::CmdBarrier(
-        cmdBuf,
-        CoreGraphics::PipelineStage::GraphicsShadersRead,
-        CoreGraphics::PipelineStage::TransferRead,
-        CoreGraphics::BarrierDomain::Global,
-        {
-            {
-                id,
-                CoreGraphics::TextureSubresourceInfo::Texture(ImageBits::ColorBits, id),
-            },
-        });
-
     // calculate number of mips
     TextureDimensions dims = TextureGetDimensions(id);
 
-    CoreGraphics::ImageLayout prevLayout = CoreGraphics::ImageLayout::TransferSource;
-    CoreGraphics::PipelineStage prevStageSrc = CoreGraphics::PipelineStage::TransferRead;
     IndexT mip;
     for (mip = 0; mip < numMips - 1; mip++)
     {
@@ -72,7 +57,7 @@ TextureGenerateMipmaps(const CoreGraphics::CmdBufferId cmdBuf, const TextureId i
         // Transition source to source
         CoreGraphics::CmdBarrier(
             cmdBuf,
-            prevStageSrc,
+            CoreGraphics::PipelineStage::AllShadersRead,
             CoreGraphics::PipelineStage::TransferRead,
             CoreGraphics::BarrierDomain::Global,
             {
@@ -85,7 +70,7 @@ TextureGenerateMipmaps(const CoreGraphics::CmdBufferId cmdBuf, const TextureId i
 
         CoreGraphics::CmdBarrier(
             cmdBuf,
-            CoreGraphics::PipelineStage::TransferRead,
+            CoreGraphics::PipelineStage::AllShadersRead,
             CoreGraphics::PipelineStage::TransferWrite,
             CoreGraphics::BarrierDomain::Global,
             {
@@ -97,24 +82,33 @@ TextureGenerateMipmaps(const CoreGraphics::CmdBufferId cmdBuf, const TextureId i
             nullptr);
         CoreGraphics::CmdBlit(cmdBuf, id, fromRegion, CoreGraphics::ImageBits::ColorBits, mip, 0, id, toRegion, CoreGraphics::ImageBits::ColorBits, mip + 1, 0);
 
-        // The previous textuer will be in write, so it needs to pingpong from transfer write/read, with the first being shader read
-        prevStageSrc = CoreGraphics::PipelineStage::TransferWrite;
-    }
-
-    // At the end, only the last mip will be in transfer write, so lets just transition that one
-    CoreGraphics::CmdBarrier(
-        cmdBuf,
-        CoreGraphics::PipelineStage::TransferWrite,
-        CoreGraphics::PipelineStage::AllShadersRead,
-        CoreGraphics::BarrierDomain::Global,
-        {
-            CoreGraphics::TextureBarrierInfo
+        CoreGraphics::CmdBarrier(
+            cmdBuf,
+            CoreGraphics::PipelineStage::TransferWrite,
+            CoreGraphics::PipelineStage::AllShadersRead,
+            CoreGraphics::BarrierDomain::Global,
             {
-                id,
-                CoreGraphics::TextureSubresourceInfo::Texture(ImageBits::ColorBits, id)
-            }
-        },
-        nullptr);
+                {
+                    id,
+                    CoreGraphics::TextureSubresourceInfo{ CoreGraphics::ImageBits::ColorBits, (uint)mip + 1, 1, 0, 1 },
+                }
+            },
+            nullptr);
+
+        // Transition source to source
+        CoreGraphics::CmdBarrier(
+            cmdBuf,
+            CoreGraphics::PipelineStage::TransferRead,
+            CoreGraphics::PipelineStage::AllShadersRead,
+            CoreGraphics::BarrierDomain::Global,
+            {
+                {
+                    id,
+                    CoreGraphics::TextureSubresourceInfo{ CoreGraphics::ImageBits::ColorBits, (uint)mip, 1, 0, 1 },
+                }
+            },
+            nullptr);
+    }
 }
 
 //------------------------------------------------------------------------------
