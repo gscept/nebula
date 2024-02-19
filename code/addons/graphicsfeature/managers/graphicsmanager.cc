47cd715a70b2960f5cc33ae368fc02dd62f13ce7
Gustav Sterbrant <gustav.sterbrant@gmail.com>
2024-02-19 23:37:13 +0100

    Use 1 byte mask for raytracing registration

--- code/addons/graphicsfeature/managers/graphicsmanager.cc
+++ code/addons/graphicsfeature/managers/graphicsmanager.cc
@@ -61,7 +61,7 @@ RegisterModelEntity(Graphics::GraphicsEntityId const gid, Resources::ResourceNam
             Models::ModelContext::SetTransform(gid, t);
             if (raytracing && CoreGraphics::RayTracingSupported)
             {
-                Raytracing::RaytracingContext::Setup(gid, CoreGraphics::BlasInstanceFlags::NoFlags, 0xFFFFFFFF);
+                Raytracing::RaytracingContext::Setup(gid, CoreGraphics::BlasInstanceFlags::NoFlags, 0xFF);
             }
             Visibility::ObservableContext::Setup(gid, Visibility::VisibilityEntityType::Model);
         }
