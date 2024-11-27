873c962d27f26a6955c8ca3a9a895b858f5cb055
Gustav Sterbrant <gustav.sterbrant@gmail.com>
2024-11-27 14:18:31 +0100

    Fix  testviewer

--- toolkit/editor/editor/ui/uimanager.cc
+++ toolkit/editor/editor/ui/uimanager.cc
@@ -163,7 +163,7 @@ UIManager::OnActivate()
 
         Graphics::GraphicsServer::SwapInfo swapInfo;
         swapInfo.syncFunc = [](CoreGraphics::CmdBufferId cmdBuf)
-    {
+    	{
             FrameScript_editorframe::Synchronize("Present_Sync", cmdBuf, CoreGraphics::GraphicsQueueType, { { (FrameScript_editorframe::TextureIndex)FrameScript_editorframe::Export_EditorBuffer.index, CoreGraphics::PipelineStage::TransferRead } }, nullptr);
         };
         swapInfo.submission = FrameScript_editorframe::Submission_EditorUI;
