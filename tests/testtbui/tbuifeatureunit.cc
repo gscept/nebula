136dbd082f8df3fced64b8755ad712da5e234ad5
Robert Campbell <waprave@gmail.com>
2024-11-27 14:22:19 -0500

    Added framescript for swapchain sync

--- tests/testtbui/tbuifeatureunit.cc
+++ tests/testtbui/tbuifeatureunit.cc
@@ -10,6 +10,8 @@
 #include "managers/inputmanager.h"
 #include "game/api.h"
 #include "game/world.h"
+#include "appgame/gameapplication.h"
+#include "frame/default.h"
 
 namespace Tests
 {
@@ -52,6 +54,32 @@ TBUIFeatureUnit::OnActivate()
     // Setup game state
     this->AttachManager(Tests::GameStateManager::Create());
     this->AttachManager(Tests::InputManager::Create());
+
+#if WITH_NEBULA_EDITOR
+    if (!App::GameApplication::IsEditorEnabled())
+#endif
+    {
+        Graphics::GraphicsServer::Instance()->AddPostViewCall(
+            [](IndexT frameIndex, IndexT bufferIndex)
+            {
+                Graphics::GraphicsServer::SwapInfo swapInfo;
+                swapInfo.syncFunc = [](CoreGraphics::CmdBufferId cmdBuf)
+                {
+                    FrameScript_default::Synchronize(
+                        "Present_Sync",
+                        cmdBuf,
+                        CoreGraphics::GraphicsQueueType,
+                        {{(FrameScript_default::TextureIndex)FrameScript_default::Export_ColorBuffer.index,
+                          CoreGraphics::PipelineStage::TransferRead}},
+                        nullptr
+                    );
+                };
+                swapInfo.submission = FrameScript_default::Submission_Scene;
+                swapInfo.swapSource = FrameScript_default::Export_ColorBuffer.tex;
+                Graphics::GraphicsServer::Instance()->SetSwapInfo(swapInfo);
+            }
+        );
+    }
 }
 
 //------------------------------------------------------------------------------
