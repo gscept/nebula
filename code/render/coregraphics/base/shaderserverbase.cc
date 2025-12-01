63cff198de1dec2d7bd228bde8de40f6ef77d692
Gustav Sterbrant <gustav.sterbrant@gmail.com>
2025-12-01 23:15:02 +0100

    Remove file watch on shaders (for now)

--- code/render/coregraphics/base/shaderserverbase.cc
+++ code/render/coregraphics/base/shaderserverbase.cc
@@ -81,90 +81,90 @@ ShaderServerBase::Open()
 
     RecursiveLoadShaders(this, "shd:");
 
-#ifndef __linux__
-    auto reloadFileFunc = [this](IO::WatchEvent const& event)
-    {
-        if (event.type == WatchEventType::Modified || event.type == WatchEventType::NameChange &&
-            !event.file.EndsWithString("TMP") &&
-            !event.file.EndsWithString("~"))
-        {
-            // remove file extension
-            Util::String out = event.file;
-            out.StripFileExtension();
-
-            // find argument in export folder
-            Ptr<IO::Stream> file = IO::IoServer::Instance()->CreateStream(Util::String::Sprintf("bin:shaders/%s.txt", out.AsCharPtr()));
-            if (file->Open())
-            {
-                System::Process process;
-
-                void* buf = file->Map();
-                SizeT size = file->GetSize();
-
-                // run process
-                Util::String cmd;
-                cmd.Set((const char*)buf, size);
-                process.SetWorkingDirectory(file->GetURI().LocalPath().ExtractDirName());
-                process.SetExecutable(cmd);
-                process.SetNoConsoleWindow(false);
-                Ptr<IO::MemoryStream> stream = IO::MemoryStream::Create();
-                process.SetStderrCaptureStream(stream);
-
-                // launch process
-                bool res = process.LaunchWait();
-                if (res)
-                {
-                    Ptr<TextReader> reader = TextReader::Create();
-                    reader->SetStream(stream);
-                    reader->Open();
-
-                    // write output from compilation
-                    while (!reader->Eof())
-                    {
-                        Core::SysFunc::DebugOut(reader->ReadLine().AsCharPtr());
-                    }
-
-                    // close reader
-                    reader->Close();
-
-                    // Get exported file name, this is what we need to reload.
-                    IndexT oIndex = cmd.FindStringIndex("-o");
-                    if (oIndex != InvalidIndex)
-                    {
-                        Util::String exportedFilePath;
-                        char const* c = cmd.AsCharPtr() + oIndex + 3; // skip "-o "
-                        while (*c != ' ')
-                        {
-                            exportedFilePath.AppendChar(*c);
-                            c += 1;
-                        }
-
-                        // reload shader
-                        this->pendingShaderReloads.Enqueue(exportedFilePath);
-                    }
-                }
-            }
-        }
-    };
-
-    // create file watcher
-    if (IO::IoServer::Instance()->DirectoryExists("home:work/shaders/vk"))
-    {
-        FileWatcher::Instance()->Watch("home:work/shaders/vk", true, IO::WatchFlags(NameChanged | SizeChanged | Write), reloadFileFunc);
-    }
-
-#ifndef PUBLIC_BUILD
-    if (System::NebulaSettings::Exists("gscept", "ToolkitShared", "path"))
-    {
-        IO::URI shaderPath = System::NebulaSettings::ReadString("gscept", "ToolkitShared", "path");
-        shaderPath.AppendLocalPath("syswork/shaders/vk");
-        if (IO::IoServer::Instance()->DirectoryExists(shaderPath))
-        {
-            FileWatcher::Instance()->Watch(shaderPath.AsString(), true, IO::WatchFlags(NameChanged | SizeChanged | Write), reloadFileFunc);
-        }
-    }
-#endif
-#endif
+//#ifndef __linux__
+//    auto reloadFileFunc = [this](IO::WatchEvent const& event)
+//    {
+//        if (event.type == WatchEventType::Modified || event.type == WatchEventType::NameChange &&
+//            !event.file.EndsWithString("TMP") &&
+//            !event.file.EndsWithString("~"))
+//        {
+//            // remove file extension
+//            Util::String out = event.file;
+//            out.StripFileExtension();
+//
+//            // find argument in export folder
+//            Ptr<IO::Stream> file = IO::IoServer::Instance()->CreateStream(Util::String::Sprintf("bin:shaders/%s.txt", out.AsCharPtr()));
+//            if (file->Open())
+//            {
+//                System::Process process;
+//
+//                void* buf = file->Map();
+//                SizeT size = file->GetSize();
+//
+//                // run process
+//                Util::String cmd;
+//                cmd.Set((const char*)buf, size);
+//                process.SetWorkingDirectory(file->GetURI().LocalPath().ExtractDirName());
+//                process.SetExecutable(cmd);
+//                process.SetNoConsoleWindow(false);
+//                Ptr<IO::MemoryStream> stream = IO::MemoryStream::Create();
+//                process.SetStderrCaptureStream(stream);
+//
+//                // launch process
+//                bool res = process.LaunchWait();
+//                if (res)
+//                {
+//                    Ptr<TextReader> reader = TextReader::Create();
+//                    reader->SetStream(stream);
+//                    reader->Open();
+//
+//                    // write output from compilation
+//                    while (!reader->Eof())
+//                    {
+//                        Core::SysFunc::DebugOut(reader->ReadLine().AsCharPtr());
+//                    }
+//
+//                    // close reader
+//                    reader->Close();
+//
+//                    // Get exported file name, this is what we need to reload.
+//                    IndexT oIndex = cmd.FindStringIndex("-o");
+//                    if (oIndex != InvalidIndex)
+//                    {
+//                        Util::String exportedFilePath;
+//                        char const* c = cmd.AsCharPtr() + oIndex + 3; // skip "-o "
+//                        while (*c != ' ')
+//                        {
+//                            exportedFilePath.AppendChar(*c);
+//                            c += 1;
+//                        }
+//
+//                        // reload shader
+//                        this->pendingShaderReloads.Enqueue(exportedFilePath);
+//                    }
+//                }
+//            }
+//        }
+//    };
+//
+//    // create file watcher
+//    if (IO::IoServer::Instance()->DirectoryExists("home:work/shaders/vk"))
+//    {
+//        FileWatcher::Instance()->Watch("home:work/shaders/vk", true, IO::WatchFlags(NameChanged | SizeChanged | Write), reloadFileFunc);
+//    }
+//
+//#ifndef PUBLIC_BUILD
+//    if (System::NebulaSettings::Exists("gscept", "ToolkitShared", "path"))
+//    {
+//        IO::URI shaderPath = System::NebulaSettings::ReadString("gscept", "ToolkitShared", "path");
+//        shaderPath.AppendLocalPath("syswork/shaders/vk");
+//        if (IO::IoServer::Instance()->DirectoryExists(shaderPath))
+//        {
+//            FileWatcher::Instance()->Watch(shaderPath.AsString(), true, IO::WatchFlags(NameChanged | SizeChanged | Write), reloadFileFunc);
+//        }
+//    }
+//#endif
+//#endif
 
     this->isOpen = true;
     return true;
