d59140cb46c48b20e156b1d6fd6f5a20a782125e
Gustav Sterbrant <gustav.sterbrant@gmail.com>
2026-04-27 17:20:23 +0200

    Loading a b4n should not trick around with subtracing 0.5, but accept signed values!

--- code/foundation/math/vec4.cc
+++ code/foundation/math/vec4.cc
@@ -36,10 +36,8 @@ vec4::load_byte4n(const void* ptr)
     __m128i vals = _mm_set1_epi32(val);
     vals = _mm_cvtepu8_epi32(vals);
     __m128 fvals = _mm_cvtepi32_ps(vals);
-    const __m128 norm = { 1.0f / 255.0f, 1.0f / 255.0f, 1.0f / 255.0f, 1.0f / 255.0f };
+    const __m128 norm = { 1.0f / 127.0f, 1.0f / 127.0f, 1.0f / 127.0f, 1.0f / 127.0f };
     fvals = _mm_mul_ps(fvals, norm);
-    fvals = _mm_add_ps(fvals, _mm_set_ps1(-0.5f));
-    fvals = _mm_mul_ps(fvals, _mm_set_ps1(2.0f));
     this->vec = fvals;
 }
 
