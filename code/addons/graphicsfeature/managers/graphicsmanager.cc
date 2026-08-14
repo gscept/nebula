3d329b0c39e955458fd4f29d7db0296c7241462f
Gustav Sterbrant <gustav.sterbrant@gmail.com>
2026-08-14 12:15:48 +0200

    Fix URN formatting

--- code/addons/graphicsfeature/managers/graphicsmanager.cc
+++ code/addons/graphicsfeature/managers/graphicsmanager.cc
@@ -58,7 +58,7 @@ RegisterModelEntity(
     Math::mat4 const& t
 )
 {
-    IO::URN modelURN(Util::Format("urn:mdl:%s", res.Value()));
+    IO::URN modelURN("mdl", res.Value());
     Models::ModelContext::RegisterEntity(gid);
     Models::ModelContext::Setup(
         gid,
