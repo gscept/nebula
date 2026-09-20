ebce1600eaac00dc1ce2b36d3aba0f1e9a50448b
Gustav Sterbrant <gustav.sterbrant@gmail.com>
2026-09-20 12:18:21 +0200

    Fix raytracing not uploading object bindings correctly due to an API change

--- code/render/raytracing/raytracingcontext.cc
+++ code/render/raytracing/raytracingcontext.cc
@@ -738,7 +738,7 @@ RaytracingContext::UpdateTransforms(const Graphics::FrameContext& ctx)
         }, entities.Size(), 1024, { &Models::ModelContext::TransformsUpdateCounter }, &idCounter, &state.jobWaitEvent);
 
         // Copy over object bindings
-        CoreGraphics::BufferUpdate(state.objectBindingBuffer.HostBuffer(), state.objects.Begin(), state.objects.ByteSize());
+        CoreGraphics::BufferUpdateArray(state.objectBindingBuffer.HostBuffer(), state.objects);
         state.topLevelNeedsUpdate = true;
     }
     else
