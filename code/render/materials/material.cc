499eac4f9444449af4865c758a302f3539c7aaab
Gustav Sterbrant <gustav.sterbrant@gmail.com>
2026-09-24 18:17:34 +0200

    Fix missing IO::Path parse in materials

--- code/render/materials/material.cc
+++ code/render/materials/material.cc
@@ -80,7 +80,7 @@ CreateMaterial(const MaterialTemplatesGPULang::Entry* entry, const Util::StringA
     for (IndexT i = 0; i < entry->textures.Size(); i++)
     {
         auto& kvp = entry->textures.KeyValuePairAtIndex(i);
-        Resources::ResourceId res = Resources::CreateResource(kvp.Value()->resource, "materials", [i, textures](Resources::ResourceId id)
+        Resources::ResourceId res = Resources::CreateResource(IO::Path::Parse(kvp.Value()->resource), "materials", [i, textures](Resources::ResourceId id)
         {
             textures[i] = id;
         });
